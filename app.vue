01de40cc7383809791923cbc89919dd7fbfa9ace
Beto Garcia <robertomedeirosgarcia@gmail.com>
2021-10-14 16:38:54 -0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import './assets/tailwind.scss'
+</script>
+
+
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<style lang="scss">
+body{
+  @apply bg-gray-800;
+}
+</style>
