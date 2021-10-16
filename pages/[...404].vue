9238171ad75f6b8658ef6033fd94f4f55091db27
Beto Garcia <robertomedeirosgarcia@gmail.com>
2021-10-16 09:40:46 -0300

    404 page

---
+++ pages/[...404].vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="w-screen h-screen flex flex-col gap-4 items-center justify-center"
+  >
+    <h1 class="text-8xl font-black text-white">404</h1>
+    <h2 class="text-4xl font-bold text-white">Page Not Found!</h2>
+    <nuxt-link
+      class="px-6 py-2 rounded-full bg-green-600 text-green-100 font-semibold"
+      to="/"
+    >
+      Back to index
+    </nuxt-link>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,5 @@
 <template>
-    <div class="text-white font-semibold p-4 flex flex-col">
-        Tailwindcss Starter
-        <a
-            target="blank"
-            class="text-green-500"
-            href="https://github.com/productfrontenddeveloper/"
-        >Follow Me Github</a>
-    </div>
+  <div class="text-white font-semibold p-4 flex flex-col">
+    Tailwindcss Starter
+  </div>
 </template>
